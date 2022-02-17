4fb9bcd43e6ffbba58734d815634ebde8c8cee61
Ele Cabrera <jai690@hotmail.com>
2022-02-16 22:52:22 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+
+// Inicialización
+const app = express();
+require('./lib/passport');
+
+
+// Settings
+app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars')
+}));
+app.set('view.engine', '.hbs');
+app.use(express.static('public'))
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+
+// Routes
+app.use(require('./routes'));
+
+// Public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Start the server
+app.listen(app.get('port'), () => {
+    console.log("Server on port", app.get('port'));
+});
